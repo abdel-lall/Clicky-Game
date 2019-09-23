9eb21ab86284d2f1df8189d1ae88567bc093c278
Abdel <abdelmounaim.lallouache@gmail.com>
2019-09-23 02:06:42 -0500

    Updates

---
+++ precache-manifest.8f12129c4d0f25e84c718fea784e66e4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93b43129e76259b417948a1ef455d0ee",
+    "url": "/clicky/index.html"
+  },
+  {
+    "revision": "a3f64ffccc3f73a2ee9d",
+    "url": "/clicky/static/js/2.d28aa7a2.chunk.js"
+  },
+  {
+    "revision": "6d75cbd9b05bf57f4dce",
+    "url": "/clicky/static/js/main.5a934929.chunk.js"
+  },
+  {
+    "revision": "05ce0870626dc13264ea",
+    "url": "/clicky/static/js/runtime-main.352fb1e4.js"
+  }
+]);
